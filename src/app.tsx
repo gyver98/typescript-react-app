0ff1a2c36d43c49fce2ecb456271a420b6c94144
Matthew Choi <matthewchoi7@gmail.com>
2017-12-13 14:15:21 +1100

    create stateful react component using typescript

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,35 @@
 import * as React from 'react';
 import * as ReactDom from 'react-dom';
 
-type AppProps = { message: string };
+type AppProps = {
+  message: string,
+}
 
-const App: React.SFC<AppProps> = ({ message }) => <div>{message}</div>;
+type AppState = {
+  count: number
+}
+
+class App extends React.Component<AppProps, AppState> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    }
+  }
+
+  increment = () => {
+    this.setState({
+      count: this.state.count + 1
+    })
+  }
+
+  render() {
+    return (
+      <div onClick={this.increment}>{this.props.message} {this.state.count}</div>
+    )
+  }
+}
 
 ReactDom.render(
-<App message="Hello typescript"/>,
+<App message={"hello typescript"}/>,
 document.getElementById('root'));
